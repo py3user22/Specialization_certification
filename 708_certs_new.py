57e7082c9abdc9318a4dff3077e4a0bacb788e19
Jesus G <113757515+py3user22@users.noreply.github.com>
2023-08-20 01:52:09 -0500

    Add files via upload
    2023june - july certificates list, long list needs to be edited.

---
+++ 708_certs_new.py
@@ -0,0 +1,92 @@
+#!/bin/python3
+
+
+June_july23_certs = {
+    "a": "--------------------------------",
+    "Google": "Architecting w/ GKE --google kubernetes engine",
+    "date 062523": "type | Specialization*",
+    "link1": "https://coursera.org/share/e2b3198aacf43f1bd9c23c4435725705",
+    "ref1 name": "EXNSBKNTSBCG",
+    "a0": "--------------------------------",
+    "InfoSec": "Windows Registry Forensics",
+    "date 070823": "type | Course",
+    "link2": "https://coursera.org/share/47a1e265ff8981af6ae8a232f855a283",
+    "ref2 name": "TGA2VHUSAZ6A",
+    "Connect Path2 Specialization name": "Computer Forensics",
+    "a1": "---------------------------------",
+    "Amazon": "AWS Fundamentals: Addressing Security Risk",
+    "date 070123": "type | Course",
+    "link3": "https://coursera.org/share/b5c4b4743c90ad0a18852ff33b36e18e",
+    "tag3 name": "ZFBZEXMPFUKB",
+    "skills3": "aws, cloud mgmt, cloud apps, info privacy",
+    "a2": "----------------------------------",
+    "ISC2": "Network Security",
+    "071223": "type | Course",
+    "link4": "https://coursera.org/share/6d622eb1187286db53aadfdf71b00859",
+    "tag4 name": "B5JZMHER94XE",
+    "Connect Path4 Specialization name": "Certified in Cybersecurity --(ISC)2",
+    "a3": "----------------------------------",
+    "InfoSec -": "Introduction to Python for cybersecurity",
+    "071623": "type | Course",
+    "link5": "https://coursera.org/share/d33c3f9b0e8046ad03a590c55d307612",
+    "tag5 name": "LMLGXYSTV7RR",
+    "Specialization connect5": "Python for cybersecurity",
+    "a4": "----------------------------------",
+    "Google -1": "Networking in Google Cloud: Hybrid connectivity & network mgmt",
+    "072123": "type | Course",
+    "link6": "https://coursera.org/share/7e40e197b6a40d8b851a103a2109826a",
+    "tag6 name": "EVZYA7EN7X73",
+    "Specialization connect6": "Networking in Google cloud",
+    "a5": "-----------------------------------",
+    "Google -2": "Preparing for your professional cloud security engineer journey",
+    "079992": "type | Course",
+    "link7": "in prog.",
+    "tag7 name": "in prog",
+    "Specialization connect7": " Preparing for Google Cloud Certification: Cloud Security Engineer Professional Certificate",
+    "a6": "-----------------------------------",
+    "aa": "",
+    "aaa": "",
+}
+
+for kee, val in June_july23_certs.items():
+    print("{}: {}".format(kee, val))
+
+
+course_trees_desc = {
+    "Name": "Description --completion date",
+    "c1-3-S": "Computer forensics specialization",
+    "c1": "Digital forensics concepts -?x",
+    "c2": "Windows OS forensics -?x",
+    "c3": "Windows registry forensics -070823",
+    "b0": "------------------",
+    "ca1-4-S": "Architecturing w/ Google Kubernetes Engine Specialization --Completed :) 062523",
+    "ca11": "EXNSBKNTSBCG | https://coursera.org/share/e2b3198aacf43f1bd9c23c4435725705",
+    "ca12": "--------------------",
+    "ca1": "Google cloud fundamentals: Core Infrastructure --061923",
+    "ca2": "Architecturing w/ Google Kubernetes Engine: Foundations --061223",
+    "ca3": "Architecturing w/ Google Kubernetes Engine: Production --062523",
+    "ca4": "Architecturing w/ Google Kubernetes Engine: Workloads --060923",
+    "b1": "------------------",
+    "cb1-5-S": "Certified in cybersecurity Specialization --(ISC)^2",
+    "cb1": "Security principles --?x",
+    "cb2": "IR incident response, BC, & DR concepts --?x",
+    "cb3": "Access control concepts --?x",
+    "cb4": "Network security --071223",
+    "cb5": "Security operations --?x",
+    "b2": "------------------",
+    "cc1-3": "Networking in Google Cloud Specialization --completed 072123",
+    "cc0": "RQP6LQ2HK89P | https://coursera.org/share/0f82452c7f6ab084cce55dd999befc9c",
+    "cc01": "-------------------",
+    "cc1": "Google Cloud Fundamentals: Core Infrastructure --061923",
+    "cc2": "Networking in Google Cloud: Defining & implementing networks --050623",
+    "cc3": "Networking in Google Cloud: Hybrid connectivity & network management --072123",
+    "cc4": "",
+    "b3": "-------------------",
+
+}
+
+print("---------------------------\n---------------------------")
+print("\tSpecialization name & subkeys\n")
+
+for ki, vaa in course_trees_desc.items():
+    print("{}: {}".format(ki, vaa))
